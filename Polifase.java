f0766c0629d9b70d1ed07281e5440571fa191be4
Monserrat L <49957927+monskysun@users.noreply.github.com>
2021-10-20 00:02:15 -0500

    Polifase separa correctamente
    Para no perder el avance en caso de que no funcione merge

--- Polifase.java
+++ Polifase.java
@@ -3,7 +3,11 @@ package EDA2Proyecto1;
 import java.util.ArrayList;
 
 public class Polifase {
-
+//falta
+	//iterar en toda la lista
+	//limpiar el original
+	//insertarlos
+	//while que repita lo anterior
 	int n = 20, i =0;
 	public void polifase(ArrayList<Persona> personas) {
 		
@@ -19,44 +23,60 @@ public class Polifase {
 		
 		ArrayList<Persona> personas1 = new ArrayList<>();
 		ArrayList<Persona> personas2 = new ArrayList<>();
+		int tamList = personas.size();
 		
-		int nF1 = 0,nF2 = 0;
+		int nF1 = 0,nF2 = 0,bloque = 0;
 		
 		Arch1.createArch("Archivo1.txt");
 		Arch2.createArch("Archivo2.txt");
 		personas = leerarch1.LecturaArch();
+		//size 1,2,3
 		
-		
-		
-			//Agrega a arch 1
-			nF1 = nF1+ LenarF1(personas,personas1);// vale 2 claves
-
+		bloque = bloque * 2;
+		while(personas.size()>0) {
+			//System.out.println("tam personas: "+personas.size());
+			if(personas.size()==1) {
+				nF1 = nF1 + LenarF1(personas,personas1);
+				
+			}else if(personas.size()>1) {
+				nF1 = nF1 + LenarF1(personas,personas1);
+				nF1 = nF1 + LenarF1(personas,personas1);
+			}
+			if(personas.size()>0) {
+				if(personas.size()==1)
+					nF2 = nF2 + LenarF2(personas,personas2);
+				else if(personas.size()>1) {
+					nF2 = nF2 + LenarF2(personas,personas2);
+					nF2 = nF2 + LenarF2(personas,personas2);
+				}
+			}
 			
-			 //Agrega a arch 2
-			    
-			nF2 = nF2 + LenarF2(personas,personas2);
-		
-			    Arch1.EscribirArchPoli(personas1,"Archivo1.txt",2);
+		} 
+	
+			    Arch1.EscribirArchPoli(personas1,"Archivo1.txt",nF1);
 				System.out.println("\nOriginal ");
 			    imprimePersona.imprimirNom(personas);
-			    Arch2.EscribirArchPoli(personas2,"Archivo2.txt",2);
+			    Arch2.EscribirArchPoli(personas2,"Archivo2.txt",nF2);
 				System.out.println("\nOriginal ");
 				System.out.println("\n Personas ="+personas.size());
 			    imprimePersona.imprimirNom(personas);
 			    
-			}
+			    
+			    
+			   
+		}
 	public int LenarF1(ArrayList<Persona> personas,ArrayList<Persona> personas1) {
 			ImprimirPersona imprimePersona = new ImprimirPersona();
 			int j =0;
 			//Agrega a arch 1
-			for(int i = 0; i<2; i++) {
-				personas1.add(personas.get(0));
-				j++;
-				personas.remove(personas.get(0));
-				}
+			
+			personas1.add(personas.get(0));
+			j=j+1;
+			personas.remove(personas.get(0));
+				
 			System.out.println("\nArch 2 sin ordenar: ");
 			imprimePersona.imprimirNom(personas1);
-		    System.out.println("\nOrdenado:");
+		    System.out.println("\nOrdenado personas 1:");
 			
 		   	//insertion.insertionSort(personas1,personas1.size());
 		    return j;
@@ -66,14 +86,14 @@ public class Polifase {
 	public int LenarF2(ArrayList<Persona> personas,ArrayList<Persona> personas2) {
 		int j =0;
 		ImprimirPersona imprimePersona = new ImprimirPersona();
-		for(int i = 0; i<2; i++) {
-			personas2.add(personas.get(0));
-			j++;
-			personas.remove(personas.get(0));
-				}
+		
+		personas2.add(personas.get(0));
+		j= j+1;
+		personas.remove(personas.get(0));
+				
 	    System.out.println("\nArch 3 sin ordenar: ");
 		imprimePersona.imprimirNom(personas2);
-	    System.out.println("\nOrdenado:");
+	    System.out.println("\nOrdenado personas 2:");
 	   // insertion.insertionSort(personas2,personas2.size());
 	    
 	    return j;
