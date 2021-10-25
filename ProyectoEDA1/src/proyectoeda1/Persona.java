0722ef3d88ced15657f856f8601b6fd3b55bf1b7
Mariela García Acosta <74431404+Mariela9302@users.noreply.github.com>
2021-10-24 21:49:29 -0500

    Comentarios Persona

--- ProyectoEDA1/src/proyectoeda1/Persona.java
+++ ProyectoEDA1/src/proyectoeda1/Persona.java
@@ -1,11 +1,11 @@
 
 package proyectoeda1;
-
+/*Se declara una clase de objetos llamada persona con sus atributos, los cuales seran, nombre, apellido, claves*/
 public class Persona {
     private String nombres;
     private String apellidos;
     private String claves;
-    
+    /*atributos del objeto*/
     public Persona( String nombres,String apellidos, String claves){ // Constructor 
     	this.nombres= nombres;
     	this.apellidos = apellidos;
@@ -14,7 +14,7 @@ public class Persona {
     public Persona(){ // Constructor 
 
     }
-      
+     /*Los metodos set permiten modificar al elemento, en este caso los set se realizan por atributo*/
     public void setNombre(String nombres) {
     	this.nombres= nombres;
     	
@@ -28,7 +28,7 @@ public class Persona {
     	this.claves= claves;
     	
     }
-    
+    /* Los metodos get permiten visusalizar al elemento, en este caso los get se realizan por atributo */
     public String getNombre() {
     	return nombres;
     }
@@ -40,7 +40,7 @@ public class Persona {
     	return claves;
     }
 
-    @Override
+    @Override /* El sigiente metodo es para poder llamar al objeto con todos sus atributos*/
     public String toString() {
         return "Persona{" + "nombres=" + nombres + ", apellidos=" + apellidos + ", claves=" + claves + '}'+" \n";
     }
