ed2ccbfc19ba9d76d9ec1432e178e6b3269c6e44
Mariela García Acosta <74431404+Mariela9302@users.noreply.github.com>
2021-10-24 19:15:22 -0500

    Comentarios Mezcla Equilibrada

--- ProyectoEDA1/src/proyectoeda1/MezclaEquilibrada.java
+++ ProyectoEDA1/src/proyectoeda1/MezclaEquilibrada.java
@@ -10,26 +10,26 @@ public class MezclaEquilibrada {
     ManejoArch mane = new ManejoArch();
     Scanner guarda = new Scanner(System.in);
     MergeSort merg = new MergeSort();
-    File archivo1= new File("f1.txt");
-    File archivo2 = new File("f2.txt");
-    File archivo0= new File("f0.txt");
-    ArrayList<ArrayList<Persona>> personas1 = new ArrayList<>();
+    File archivo1= new File("f1.txt"); //archivo auxiliar 1
+    File archivo2 = new File("f2.txt"); //archivo auxiliar 2
+    File archivo0= new File("f0.txt"); //archivo que sustituira al original para que el usario pueda visualizar el cambio
+    ArrayList<ArrayList<Persona>> personas1 = new ArrayList<>(); // listas de listas para poder hacer las fusiones con las posiciones correctas (lista prinicipal)
     ArrayList<ArrayList<Persona>> personas2 = new ArrayList<>();
-    ArrayList<ArrayList<Persona>> personas3= new ArrayList<>();
-    ArrayList<Persona> pers1 = new ArrayList<>();
+    ArrayList<ArrayList<Persona>> personas3= new ArrayList<>(); 
+    ArrayList<Persona> pers1 = new ArrayList<>(); // la lista de lista (lista secundaria)
     ArrayList<Persona> pers2 = new ArrayList<>();
     ArrayList<Persona> pers3 = new ArrayList<>();
     
-    public void mezclEqui(ArrayList<Persona> personas) {
+    public void mezclEqui(ArrayList<Persona> personas) { // metodo que manda llamar todos los metodos abajo descritos, por el momento solo realiza dos iteraciones.
 	mane.EscribirArch(personas,"f0.txt");
         
-        dividir(personas,archivo1,archivo2);
+        dividir(personas,archivo1,archivo2); //inicio de oteración 1 
         dividirPersonasx(personas1,personas2,personas3);
         
         imprimePersona.imprimirListList(personas3);
         pers3=mane.LecturaArch("f0.txt");
         
-        dividir(pers3,archivo1,archivo2);
+        dividir(pers3,archivo1,archivo2); // inicio de iteración 2
         
         dividirPersonasx(personas1,personas2,personas3);
         imprimePersona.imprimirListList(personas3);
@@ -37,31 +37,34 @@ public class MezclaEquilibrada {
     }
     
     public void dividir(ArrayList<Persona> list, File archF1, File archF2){
-        ManejoArch.borrar("f1.txt");
+	/* Realiza el encapsualamiento de los bloques ordenados e intercala entre un archivo y otro */
+        ManejoArch.borrar("f1.txt"); //como se trabara con archivos auxiliares en cada iteración deben ser borrados o vaciados 
         ManejoArch.borrar("f2.txt");
-        personas1.clear();
+        personas1.clear(); // al igual que las listas deben estar vacías para evitar sobreescribir
         personas2.clear();
         int i=0;
-        boolean noOrdenado=false;
-        String inicio = "";
-        Persona auxiliar;
+        boolean noOrdenado=false; //metodo que nos permitira validar si estan ordenados o no, 
+	/* Tiene una función importante ya que funciona como un apagador, dependiendo de si es true o false, será el archivo donde se almacenaran los bloques */ 
+        String inicio = ""; //se trabajara con cadenas por ello es necesario inicializar la variable string con una cadena vacía 
+        Persona auxiliar; // todo un objeto sera almacenado en auxiliar
         
-        while(i<list.size()){
-            String valor=list.get(i).getApellidos();
-            auxiliar=list.get(i);
+        while(i<list.size()){ //mientras la lista sea menor a el tamaño de la lista 
+            String valor=list.get(i).getApellidos(); // valor sera igual al apellido (atributo del objeto)
+            auxiliar=list.get(i); //auxiliar guarda al objeto completo
             
-            if(valor.compareTo(inicio)>0){
-                inicio=valor;
-                if(!noOrdenado){
-                    pers1.add(auxiliar);
-                    
+            if(valor.compareTo(inicio)>0){ //compara el valor con el inicio, por ejemplo si nuestro objeto 0 es Muñoz entonces Muños es mayor que 0?
+		    /* como la respuesta es afirmativa entra en este if */
+                inicio=valor; //modifica el valor del incio por lo que ahora inicio es igual a muñoz
+                if(!noOrdenado){ //si esta ordenado entonces 
+                    pers1.add(auxiliar); // agrega la persona completa a la persona 1
+                    /*Condicion que solo se cumple con el ultimo elemento*/
                     if(i+1==list.size()){                                   //Condición para la última iteración
-                        ArrayList<Persona> pers3 = new ArrayList<>(pers1);  
+                        ArrayList<Persona> pers3 = new ArrayList<>(pers1); 
                         if(!pers3.isEmpty()){
-                            personas1.add(pers3);
+                            personas1.add(pers3); 
                         }
                         pers1.clear();
-                    }
+                    }/*Fin condicion*/
                 }else if(noOrdenado){
                     pers2.add(auxiliar);
                     if(i+1==list.size()){                                   //Condición para la última iteración
@@ -72,20 +75,20 @@ public class MezclaEquilibrada {
                         pers2.clear();
                     }
                 }
-            }else{
-                inicio=valor;
+            }else{ // si el valor evaluado no es mayor entonces 
+                inicio=valor; // modifica el valor de inicio
                 ArrayList<Persona> pers3 = new ArrayList<>(pers1);      //Copia y vaciado de la primera lista
-                if(!pers3.isEmpty()){
-                    personas1.add(pers3);
+                if(!pers3.isEmpty()){ //si no enta vacía entonces 
+                    personas1.add(pers3); //añade a la lista de listas el elemento 
                 }
-                pers1.clear();
-                
+                pers1.clear(); //limpia la lista principal 
+                /*Realiza lo mismo que en el if pero esta se utiliza para el segundo archivo */
                 ArrayList<Persona> pers4 = new ArrayList<>(pers2);      //Copia y vaciado de la segunda lista tras ingresarse a la lista de listas
                 if(!pers4.isEmpty()){
                     personas2.add(pers4);
                 }
                 pers2.clear();
-                
+                /* permite intercalar los archivos */
                 if(!noOrdenado){ //!true-> false
                     pers2.add(auxiliar);
                     noOrdenado=true;
@@ -94,7 +97,7 @@ public class MezclaEquilibrada {
                     noOrdenado=false;
                 } 
             }
-            i+=1;
+            i+=1; // iterador para avanzar de objeto en objeto
         }
         if(!personas1.isEmpty()){           //Vaciado de la lista 1 de listas de personas en el archivo 1 (F1)
             if(pers1.size()==0){
@@ -117,6 +120,9 @@ public class MezclaEquilibrada {
     }
     
     public void dividirPersonasx(ArrayList< ArrayList<Persona>> Personasix,ArrayList<ArrayList<Persona>> Personasiy,ArrayList<ArrayList<Persona>> Personasfy) {
+	    /* Metodo que permite fusionar las listas de listas*/
+	    /* Se usaron listas de listas para que los bloques sean ordenados con su igual es decir la lista de listas 1 en la posicion 0 se fusionara con la lista de listas 2 
+	    en la posicion 0*/
         ManejoArch.borrar("f0.txt");
         Personasfy.clear();
         while(Personasix.size()>0||Personasiy.size()>0) {
