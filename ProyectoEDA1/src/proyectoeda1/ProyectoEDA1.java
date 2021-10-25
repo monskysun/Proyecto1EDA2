d9b9179a4af0411dc38820289bec5e83d8db831f
Mariela García Acosta <74431404+Mariela9302@users.noreply.github.com>
2021-10-24 21:52:48 -0500

    Comentarios y Menú actualizados

--- ProyectoEDA1/src/proyectoeda1/ProyectoEDA1.java
+++ ProyectoEDA1/src/proyectoeda1/ProyectoEDA1.java
@@ -19,34 +19,36 @@ public class ProyectoEDA1 {
         System.out.println("\t\tBIENVENIDO\n");
         System.out.print("Introduzca el nombre del archivo:  (Archivo.txt) "); 
         String nombreArchivo=guarda.nextLine();                                  
-        
+        /* Llamada a clases */
         //poli.polifase2(personas);
-        
         mezc.mezclEqui(leerarch1.LecturaArch(nombreArchivo));
         //rdx.radixExtrn(leerarch1.LecturaArch(nombreArchivo));
                 
-	//while(opcion!=4){
-//          System.out.println("\nSelecciona un algoritmo de ordenamiento externo");
-//          System.out.println("1)Polifase  2) Mezcla 3)Radix Sort  4)Salir");
-//		//opcion=guarda.nextInt();
-//		   
-//		        
-//          switch(opcion){ 
-//			case 1:// Poli
-//		                break;
-//			case 2:// Mezcla
-//                                mezc.mezclEqui(leerarch1.LecturaArch(nombreArchivo));
-//		                break;
-//			case 3:// Radix	
-//		            rdx.radixExtrn(leerarch1.LecturaArch(nombreArchivo));
-//		                break;
-//			case 4:// salir		
-//		                break;
-//			default:
-//			    System.out.println("\tSELECCIONA UNA OPCIÓN VÁLIDA\n"); 
-//			             
-//			}
-            //}
-		System.out.println("\n\tHASTA LUEGO:)\n");
+	 /*            
+	while(opcion!=4){
+          System.out.println("\nSelecciona un algoritmo de ordenamiento externo");
+          System.out.println("1)Polifase  \n2) Mezcla \n3)Radix Sort  \n4)Salir");
+          opcion=guarda.nextInt();
+	  /*Dentro de los cases se hace el llamado al metodo */	
+	    /*   	        
+        switch(opcion){ 
+			case 1:// Poli
+				poli.polifase2(personas);
+		    break;
+			case 2:// Mezcla
+              mezc.mezclEqui(leerarch1.LecturaArch(nombreArchivo));
+		    break;
+			case 3:// Radix	
+		        rdx.radixExtrn(leerarch1.LecturaArch(nombreArchivo));
+		    break;
+			case 4:// salir		            
+			break;
+			default:
+			    System.out.println("\tSELECCIONA UNA OPCIÓN VÁLIDA\n"); 
+			break;
+			}  
+		
+       System.out.println("\n\tHASTA LUEGO:)\n");
+			}*/
 			}
 }
